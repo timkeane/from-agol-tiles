dea665bd1e98fbc1303cae270f3f09eb020eb162
Tim Keane <t.d.keane@gmail.com>
2023-11-01 08:27:21 -0400

    mvtBasemap

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import GeoJSON from 'ol/format/GeoJSON';
 import Style from 'ol/style/Style';
 import Stroke from 'ol/style/Stroke';
 import proj4 from 'proj4';
-import {register} from 'ol/proj/proj4';
 import {mvtBasemap} from './fromAGOL.mjs';
+import {register} from 'ol/proj/proj4';
 
 proj4.defs([
   ['EPSG:2263', '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=ft +to_meter=0.3048006096012192 +no_defs'],
