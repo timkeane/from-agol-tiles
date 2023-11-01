05b9941de18f921e2f0c7e451e3c597058e28a8c
Tim Keane <t.d.keane@gmail.com>
2023-11-01 08:08:19 -0400

    projection from service definition

--- fromAGOL.mjs
+++ fromAGOL.mjs
@@ -1,16 +1,17 @@
 import TileGrid from 'ol/tilegrid/TileGrid';
-import VectorTileSource from 'ol/source/VectorTile';
-import VectorTileLayer from 'ol/layer/VectorTile';
+import Source from 'ol/source/VectorTile';
+import Layer from 'ol/layer/VectorTile';
 import MVT from 'ol/format/MVT';
 import {applyStyle, applyBackground} from 'ol-mapbox-style';
 
-export function tileGrid(serviceUrl) {
+export function tileGridInfo(serviceUrl) {
   return new Promise((resolve, reject) => {
     fetch(serviceUrl).then(response => {
       response.json().then(serviceDef => {
         const esriExtent = serviceDef.fullExtent;
         const extent = [esriExtent.xmin, esriExtent.ymin, esriExtent.xmax, esriExtent.ymax];
         const tileInfo = serviceDef.tileInfo;
+        const projection = `EPSG:${tileInfo.spatialReference.latestWkid}`;
         const origin = [tileInfo.origin.x, tileInfo.origin.y];
         const lods = tileInfo.lods;
         const tileSize = [tileInfo.cols, tileInfo.rows];
@@ -18,7 +19,8 @@ export function tileGrid(serviceUrl) {
         lods.forEach(lod => {
           resolutions.push(lod.resolution);
         });
-        resolve(new TileGrid({origin, extent, tileSize, resolutions}));
+        const tileGrid = new TileGrid({origin, extent, tileSize, resolutions});
+        resolve({tileGrid, projection});
       });
     }).catch(err => {
       console.error(`Failed to create TileGid from service ${serviceUrl}`, err)
@@ -28,26 +30,27 @@ export function tileGrid(serviceUrl) {
 
 export function  mvtLayer(serviceUrl) {
   return new Promise((resolve, reject) => {
-    tileGrid(serviceUrl).then(tileGrid => {
-      const layer = new VectorTileLayer({
-        declutter: true,
-        source: new VectorTileSource({
-          format: new MVT(),
-          projection: 'EPSG:2263',
-          tileGrid,
-          url: `${serviceUrl}/tile/{z}/{y}/{x}.pbf`
-        })
-      });
+    tileGridInfo(serviceUrl).then(gridInfo => {
+      const tileGrid = gridInfo.tileGrid;
       const styleUrl =`${serviceUrl}/resources/styles/root.json`;
       const options =  {
         resolutions: tileGrid.getResolutions(),
         updateSource: false
       };
+     const layer = new Layer({
+        declutter: true,
+        source: new Source({
+          format: new MVT(),
+          projection: gridInfo.projection,
+          tileGrid: tileGrid,
+          url: `${serviceUrl}/tile/{z}/{y}/{x}.pbf`
+        })
+      });
       applyStyle(layer, styleUrl, '', options);
       applyBackground(layer, styleUrl);
       resolve(layer);
     }).catch(err => {
-      console.error(`Failed to create VectorTileLayer from service ${serviceUrl}`, err)
+      console.error(`Failed to create Layer from service ${serviceUrl}`, err)
     });
   });
 }
