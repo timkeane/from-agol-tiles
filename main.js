325da363874369eda3c6f5100510c4477d2f604a
timkeane <t.d.keane@gmail.com>
2023-11-16 11:05:19 -0500

    .

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ import Style from 'ol/style/Style';
 import Stroke from 'ol/style/Stroke';
 import {createBasemap, createLayer} from './fromAgolTiles';
 import proj4 from 'proj4';
-import {register} from 'ol/proj/proj4';
 
 proj4.defs([
   ['EPSG:2263', '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=ft +to_meter=0.3048006096012192 +no_defs']
 ]);
-register(proj4);
 
 const vectorTileServiceUrl = 'https://tiles.arcgis.com/tiles/yG5s3afENB5iO9fj/arcgis/rest/services/Poletop_2263/VectorTileServer/';
 const imageTileServiceUrl = 'https://tiles.arcgis.com/tiles/yG5s3afENB5iO9fj/arcgis/rest/services/NYC_Orthos_-_2020/MapServer/';
